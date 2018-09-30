fcb8cf21542848be16a812fd44d0c64bca6dfe46
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2018-09-30 14:50:28 -0400

    class scores project finished

--- Python Udemy Class Final Project.py
+++ Python Udemy Class Final Project.py
@@ -7,28 +7,75 @@
 #Rewards in tuple
 #Condition checks for students meeting standards
 
-print("Hello, teacher. Here is a report of your students and their rewards.")
+import operator #per google search
 
-#made-up grade totals for each student. dictionary.
-studentGrades = {"Gregg": 1050, "Rachel": 980, "Jeff": 955, "Merry": 1200,
-                 "Chris": 600, "Ellen": 990, "Michael": 880}
-#tuple for unchanging rewards
-cashRewards = (500, 300, 100)
+print("Hello, teacher. Let's figure out your students' rewards.")
+print()
 
-#confirm it's correct
-print(studentGrades)
-print(cashRewards)
+def readStudentDetails():
+    #made up grades: {"Gregg": 1050, "Rachel": 980, "Jeff": 955, "Merry": 1200,
+                 #"Chris": 600, "Ellen": 990, "Michael": 880}
+    print("Enter the number of students: ")
+    numberOfStudents = int(input())
+    studentRecord = {} #the dictionary of student marks
+    for student in range(0, numberOfStudents):
+        print("Enter the name of the student: ")
+        name = input()
+        print("Enter the final score of the student: ")
+        marks = int(input())
+        studentRecord[name] = marks
+        print()
+    return studentRecord
 
-#sort them by value, then reverse results
-studentGrades = sorted(studentGrades.values(), reverse=True)
+def rankStudents(studentRecord):
+    #you can't sort a dictionary for some reason.
+    #so convert the dictionary into something else. a tuple.
+    #solution with help from google search:
+    try:
+        sortedStudentRecord = sorted(studentRecord.items(),
+                                     key = operator.itemgetter(1), reverse = True)
+        print(sortedStudentRecord)
+        print("{} is ranked first, with a grade of {}".format
+              (sortedStudentRecord[0][0], sortedStudentRecord[0][1]))
+        print("{} is ranked second, with a grade of {}".format
+              (sortedStudentRecord[1][0], sortedStudentRecord[1][1]))
+        print("{} is ranked third, with a grade of {}".format
+              (sortedStudentRecord[2][0], sortedStudentRecord[2][1]))
+        #those each are first the index position of the pair,
+        #then the index position of the name and grade (ie, name always 0, grade always 1)
+        print()
+        return sortedStudentRecord
+    except IndexError:
+        print("Please enter at least 3 students on your next try.")
+        quit()
+    
+def rewardStudents(sortedStudentRecord, reward):
+    print("{} has received a cash reward of ${}".format
+          (sortedStudentRecord[0][0], reward[0]))
+    print("{} has received a cash reward of ${}".format
+          (sortedStudentRecord[1][0], reward[1]))
+    print("{} has received a cash reward of ${}".format
+          (sortedStudentRecord[2][0], reward[2]))
+    print()
 
-#confirm that worked
-print(studentGrades)
-
-for value in studentGrades.items():
-    if value >= 950:
-        print("Good job, {}. You did well!", studentGrades.keys)
-    else:
-        print("You need to improve, {}", studentGrades.keys)
+def congratStudents(sortedStudentRecord):
+    for record in sortedStudentRecord:
+        if record[1] >= 950:
+            print("Congrats on scoring a grade of {}, {}.".format
+                  (record[1], record[0]))
+        else:
+            break
+    print()
+    
+#create the whole list of students and their grades by calling readStudentDetails
+studentRecord = readStudentDetails()
+#create a ranked list of students by passing the whole list to the rankStudents function
+sortedStudentRecord = rankStudents(studentRecord)
+#create a tuple for the rewards (tuples cannot be changed)
+reward = (500, 300, 100)
+#pass both sorted list of students and reward info to rewardStudents function
+rewardStudents(sortedStudentRecord, reward)
+#now let's congrat the students
+congratStudents(sortedStudentRecord)
 
 
